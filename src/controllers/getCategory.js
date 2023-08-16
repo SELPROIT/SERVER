68c797d5d501174e21d704af0bd88e65a47cdaa8
JalfMVZ <velascojosealfredo1@gmail.com>
2023-08-16 11:18:25 -0500

    getCat

---
+++ src/controllers/getCategory.js
@@ -0,0 +1,9 @@
+
+const { Category } = require('../db');
+
+const getAllCategory = async () => {
+    const categories = await Category.findAll();
+    return categories;
+}
+
+module.exports = getAllCategory;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const { toRegister } = require('../handlers/authHandler');
+const { toCategory } = require('../handlers/catHandler');
 
 const router = Router();
+
+
+router.get("/categories", toCategory);
 
 router.post("/register", toRegister);
 // router.use("/login", );
