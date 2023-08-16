d1b10a3ca0f08dcde2699257cdc0f95c88773b6b
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-08-16 18:45:12 -0300

    getShopHandler

---
+++ src/handlers/getShopHandler.js
@@ -0,0 +1,21 @@
+const {getShop} = require("../controllers/getShopController");
+const {responseObj} = require("./response")
+
+const getAllUsers = async (req, res) => {
+
+    try {
+        const shops = await getShop(); 
+
+        if (shops.length === 0) {
+            res.status(404).json(responseObj('No se encontró ninguna puja para este producto.'));
+        } else {
+            res.status(200).json(responseObj('Estas son las pujas de este producto.', shops)); 
+        }
+    } catch (error) {
+        res.status(400).json(responseObj(error.message));
+    }
+};
+
+module.exports = {
+    getAllUsers
+};
