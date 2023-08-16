a61eb884095442bcf4f06df0fc96b7baf04e5453
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-08-16 19:50:39 -0300

    actualizando el controller de shop

--- src/controllers/getShopController.js
+++ src/controllers/getShopController.js
@@ -1,14 +1,20 @@
-const { Shop, Product } = require("../db");
+const { Shop, Product, User } = require("../db");
 
 const getShop = async () => {
 
   const shops = await Shop.findAll({
-    // include: {
-    //   model: Product, 
-    //   through: {
-    //     attributes: []
+    // include: [
+    //   {
+    //     model: Product,
+    //     through: {
+    //       attributes: []
+    //     }
+    //   },
+    //   {
+    //     model: User, 
+    //     attributes: ['num_ident'] //muestro sólo el número de identidad del usuario en la puja
     //   }
-    // },
+    // ]
   });
 
   return shops;
