077b58a0e59dc6e9bd969f86a39d8f3322427f3f
Julita <julia.franchi7@gmail.com>
2023-08-16 14:21:52 -0300

    postUser whith auth in handler

--- src/handlers/UserToPost.js
+++ src/handlers/UserToPost.js
@@ -1,5 +1,7 @@
 const postUser = require('../controllers/PostUser');
+const { register } = require('../controllers/authController'); // Importa la función register
 const responseObj = require('../handlers/response');
+
 const toPostUser = async (req, res) => {
 	try {
 		const {
@@ -26,33 +28,41 @@ const toPostUser = async (req, res) => {
 			curr_auc,
 			favorites,
 		} = req.body;
-		const newUser = await postUser({
-			name,
-			num_ident,
-			user_name,
-			password,
-			supplier,
-			RUT,
-			RUT_image,
-			commerce_chamber,
-			legal_ident,
-			commercial_references,
-			sector,
-			CIIU,
-			phone,
-			email,
-			id_subcat,
-			adress,
-			interaction_history,
-			buy_history,
-			offers_history,
-			win_history,
-			curr_auc,
-			favorites,
-		});
-		res.status(200).json(responseObj('Usuario creado con exito', newUser));
+
+		result = await register(user_name, password);
+		if (!result) {
+			const newUser = await postUser({
+				name,
+				num_ident,
+				user_name,
+				password,
+				supplier,
+				RUT,
+				RUT_image,
+				commerce_chamber,
+				legal_ident,
+				commercial_references,
+				sector,
+				CIIU,
+				phone,
+				email,
+				id_subcat,
+				adress,
+				interaction_history,
+				buy_history,
+				offers_history,
+				win_history,
+				curr_auc,
+				favorites,
+			});
+
+			res.status(200).json(responseObj('Usuario creado con éxito', newUser));
+		}
 	} catch (error) {
-		res.status(404).json(responseObj());
+		res
+			.status(400)
+			.json(responseObj('Error al crear el usuario', null, error.message));
 	}
 };
+
 module.exports = toPostUser;
