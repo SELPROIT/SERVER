3a414d46a99c9de6f566657a4788e2d0274070b1
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-08-17 19:58:30 -0300

    actualizando relaciones de getAuctionBid

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,12 @@ const prodRoute = require('./getAllProdR');
 const { toRegister } = require('../handlers/authHandler');
 const { toCategory } = require('../handlers/catHandler');
 const routerUser = require('./UserRouterPost');
-const bidsRouter = require("./getAuctionBidsRoutes");
+
 const router = Router();
 
 router.use('/home', prodRoute);
 router.get('/categories', toCategory);
 router.post('/register', toRegister);
 router.use('/user', routerUser);
-router.use("/bids", bidsRouter);
+
 module.exports = router;
