b07cf53ffb4594ea49dc44d3703e790635bc7b67
Julita <julia.franchi7@gmail.com>
2023-08-17 00:24:49 -0300

    cloudinary funcionando ;)

--- src/controllers/PostUser.js
+++ src/controllers/PostUser.js
@@ -11,33 +11,39 @@ cloudinary.config({
 });
 
 const postUser = async (newUser) => {
-	const result = await cloudinary.uploader.upload(newUser.RUT_image, {
-		folder: 'user-documents',
-	});
-	const user = await User.create({
-		name: newUser.name,
-		num_ident: newUser.num_ident,
-		user_name: newUser.user_name,
-		password: newUser.password,
-		supplier: newUser.supplier,
-		RUT: newUser.RUT, //numero de identificacion empresarial
-		RUT_image: result.secure_url, //cloudinary imagen de documento legal.
-		commerce_chamber: newUser.commerce_chamber,
-		legal_ident: newUser.DNI,
-		commercial_references: newUser.commercial_references,
-		sector: newUser.sector,
-		CIIU: newUser.CIIU,
-		phone: newUser.phone,
-		email: newUser.email,
-		id_subcat: newUser.id_subcat,
-		adress: newUser.adress,
-		interaction_history: [],
-		buy_history: [],
-		offers_history: [],
-		win_history: [],
-		curr_auc: [],
-		favorites: [],
-	});
-	return user;
+	try {
+		const result = await cloudinary.uploader.upload(newUser.RUT_image, {
+			folder: 'selpro/user-documents',
+		});
+
+		const user = await User.create({
+			name: newUser.name,
+			num_ident: newUser.num_ident,
+			user_name: newUser.user_name,
+			password: newUser.password,
+			supplier: newUser.supplier,
+			RUT: newUser.RUT,
+			RUT_image: result.secure_url,
+			commerce_chamber: newUser.commerce_chamber,
+			legal_ident: newUser.DNI,
+			commercial_references: newUser.commercial_references,
+			sector: newUser.sector,
+			CIIU: newUser.CIIU,
+			phone: newUser.phone,
+			email: newUser.email,
+			id_subcat: newUser.id_subcat,
+			adress: newUser.adress,
+			interaction_history: [],
+			buy_history: [],
+			offers_history: [],
+			win_history: [],
+			curr_auc: [],
+		});
+
+		return user;
+	} catch (error) {
+		throw error;
+	}
 };
+
 module.exports = postUser;
