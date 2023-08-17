11bdd13a02d2f6ccf0d1968380af6a27ef70aee7
AVaamondeH <alexxherrera2772@gmail.com>
2023-08-16 21:05:40 -0500

    Relaciones & mod models

--- src/models/auction_bid.js
+++ src/models/auction_bid.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('Shop', {
+    sequelize.define('Auction_bid', {
         id: {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
