fc16f1f026373f98e69d88f2e28eb9205713a1ab
AVaamondeH <alexxherrera2772@gmail.com>
2023-08-16 20:02:06 -0500

    model invert_auction

---
+++ src/models/invert_auctions.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Invert_auction', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        // product_name: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        // },
+        base_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        target_quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        total: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        base_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        close_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        invert: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    },
+        {
+            timestamps: false,
+        }
+    );
+};
+
