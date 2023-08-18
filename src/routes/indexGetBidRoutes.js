61a178646f3ebff8a686c5600c2165b8d2c6938c
Alex Vaamonde <128858520+AVaamondeH@users.noreply.github.com>
2023-08-17 22:30:48 -0500

    Revert "User admin"

--- src/routes/indexGetBidRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const bidsRouter = require("./getAuctionBidsRoutes");
-
-const router = Router();
-
-router.use("/bids", bidsRouter);
-
-module.exports = router;
