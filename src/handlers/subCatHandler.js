7aaee2eb10ea88c3a460fe3b986baede078075ea
EdgarGoPr <egdeprada@hotmail.com>
2023-08-17 22:15:37 -0300

    fix server

--- src/handlers/subCatHandler.js
+++ src/handlers/subCatHandler.js
@@ -1,10 +1,10 @@
-const { getAllSubCategory } = require('../controllers/subCategory.js');
+const { getAllSubCategories  } = require('../controllers/getSubCategory');
 const responseObj = require('./response.js')
 
 
 async function toSubCategory(req, res) {
     try {
-        const subCategories = await getAllSubCategory();
+        const subCategories = await getAllSubCategories();
         res.status(200).json(responseObj("Funciona", subCategories));
     } catch (error) {
         return res.status(500).json(responseObj);
