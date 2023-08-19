0fc277b6b9ff169df1f19a774bd7efaee7654c45
EdgarGoPr <egdeprada@hotmail.com>
2023-08-19 16:55:21 -0300

    invertauction put

--- src/routes/putRoutes.js
+++ src/routes/putRoutes.js
@@ -1,4 +1,5 @@
 const { put_auc_handler } = require('../handlers/put_auction_handler')
+const { put_inv_auc_handler } = require('../handlers/put_inv_auction_handler')
 const { put_prod_handler } = require('../handlers/put_prod_handler')
 
 
@@ -6,5 +7,6 @@ const putRoutes = require('express').Router()
 
 putRoutes.put('/product/:id', put_prod_handler)
 putRoutes.put('/auction/:id', put_auc_handler)
+putRoutes.put('/invertAuction/:id', put_inv_auc_handler)
 
 module.exports = putRoutes
