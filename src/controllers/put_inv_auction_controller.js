8a7adcb75d3f993c1eddab5b2c1ad8a6480d59e0
EdgarGoPr <egdeprada@hotmail.com>
2023-08-20 19:28:20 -0300

    fix put invauc and auc

--- src/controllers/put_inv_auction_controller.js
+++ src/controllers/put_inv_auction_controller.js
@@ -2,27 +2,38 @@ const { Invert_auction } = require('../db')
 
 const put_inv_auc_controller = async (
     id,
-    base_price, 
-    target_quantity, 
-    total, 
-    close_date
+    base_price,
+    target_quantity,
+    total,
+    close_date,
+    deleteFlag,
+    authorize
 ) => {
-    console.log('id', id)
     const inv_auction = await Invert_auction.findOne({ where: { id: id } })
     if (!inv_auction) 'Inverted auction not found'
 
     const changed_inv_auction = {}
-    if (base_price) {
+    if (!!base_price) {
         changed_inv_auction.base_price = base_price
     }
-    if (target_quantity) {
+    if (!!target_quantity) {
         changed_inv_auction.target_quantity = target_quantity
     }
-    if (total) {
+    if (!!total) {
         changed_inv_auction.total = total
     }
-    if (close_date) {
-        changed_inv_auction.close_date = close_date
+    if (close_date !== undefined) {
+        const parsedCloseDate = new Date(close_date);
+        if (isNaN(parsedCloseDate)) {
+            throw new Error('Invalid date format for close_date');
+        }
+        changed_inv_auction.close_date = parsedCloseDate;
+    }
+    if (deleteFlag !== undefined || deleteFlag !== null) {
+        changed_inv_auction.deleteFlag = deleteFlag
+    }
+    if (authorize !== undefined || authorize !== null) {
+        changed_inv_auction.authorize = authorize
     }
 
     const [updatedRows] = await Invert_auction.update(changed_inv_auction, {
--- src/controllers/put_prod_controller.js
+++ src/controllers/put_prod_controller.js
@@ -18,33 +18,29 @@ const put_prod_controller = async (
 
   const changed_product = {};
 
-  switch (true) {
-    case !!name:
-      changed_product.name = name;
-      break;
-    case !!brand:
-      changed_product.brand = brand;
-      break;
-    case !!image:
-      changed_product.image = image;
-      break;
-    case !!description:
-      changed_product.description = description;
-      break;
-    case !!datasheet:
-      changed_product.datasheet = datasheet;
-      break;
-    case !!rating:
-      changed_product.rating = rating;
-      break;
-    case !!stock:
-      changed_product.stock = stock;
-      break;
-    case !!price:
-      changed_product.price = price;
-      break;
-    default:
-      break;
+  if (!!name) {
+    changed_product.name = name;
+  }
+  if (!!brand) {
+    changed_product.brand = brand;
+  }
+  if (!!image) {
+    changed_product.image = image;
+  }
+  if (!!description) {
+    changed_product.description = description;
+  }
+  if (!!datasheet) {
+    changed_product.datasheet = datasheet;
+  }
+  if (!!rating) {
+    changed_product.rating = rating;
+  }
+  if (!!stock) {
+    changed_product.stock = stock;
+  }
+  if (!!price) {
+    changed_product.price = price;
   }
 
   const [updatedRows] = await Product.update(changed_product, {
