128e1fc01f37d3fb4d73717271058186be036d11
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-08-23 18:37:32 -0300

    agregando validaciones

--- src/db.js
+++ src/db.js
@@ -3,11 +3,16 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/selpro`, {
-    logging: false,
-    native: false,
+const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`, {
+	logging: false,
+	native: false,
+	dialectOptions: {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	}
 });
 
 const basename = path.basename(__filename);
