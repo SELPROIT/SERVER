78c401acf1ebe92ecc84bd7575bfbf725776e1fc
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-08-27 21:06:03 -0300

    arreglando errores de product_name

--- src/controllers/get/get_auction_by_id_controller.js
+++ src/controllers/get/get_auction_by_id_controller.js
@@ -1,24 +1,44 @@
-const { Auction } = require('../../db');
-
+const { Auction } = require("../../db");
 
 const get_auction_by_id = async (auction_id) => {
-    const { id, image, product_name, brand, description, datasheet, total, base_price, close_date, ProductId } = await Auction.findByPk(auction_id)
+  const {
+    id,
+    base_price,
+    close_date,
+    product,
+    user,
+    authorize,
+    image,
+    product_name,
+    brand,
+    description,
+    datasheet,
+    status,
+    total,
+    type,
+    auction_bids
+  } = await Auction.findByPk(auction_id);
 
-    const response = {
-        id,
-        product_id: ProductId,
-        image: image,
-        name: product_name,
-        brand: brand,
-        description: description,
-        datasheet: datasheet,
-        total: total,
-        base_price,
-        close_date
-    }
-    return response
-}
+  const response = {
+    id,
+    base_price,
+    close_date,
+    product,
+    user,
+    authorize,
+    image,
+    product_name,
+    brand,
+    description,
+    datasheet,
+    status,
+    total,
+    type,
+    auction_bids
+  };
+  return response;
+};
 
 module.exports = {
-    get_auction_by_id,
-}
+  get_auction_by_id
+};
