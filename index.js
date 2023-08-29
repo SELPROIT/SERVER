7fdbd7d3ee348b27fb31e4f829f684ed5565e057
EdgarGoPr <egdeprada@hotmail.com>
2023-08-29 13:38:35 -0300

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8080
 
 conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
