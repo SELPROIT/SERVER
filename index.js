f621f8fa0ba39910afe39149bc0ee5d11601b340
EdgarGoPr <egdeprada@hotmail.com>
2023-08-29 15:15:35 -0300

    change port

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.PORT || 8080
-const host = "0.0.0.0"
+const port = process.env.PORT ?? 8080
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(port, host, () => {
+  server.listen(port, () => {
     console.log(`%s listening at ${port}`);
   });
 }); 
