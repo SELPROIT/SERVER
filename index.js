1cee67c0552e061348452d2b84a0b1bc490e6b12
EdgarGoPr <egdeprada@hotmail.com>
2023-08-29 14:37:55 -0300

    change host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 const port = process.env.PORT || 8080
-const host = "10.18.83.41"
+const host = "0.0.0.0"
 
 conn.sync({ alter: true }).then(() => {
   server.listen(port, host, () => {
