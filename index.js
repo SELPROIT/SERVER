c5720423892591cb4e6caec91e0da31be8acf5ee
EdgarGoPr <egdeprada@hotmail.com>
2023-08-29 13:45:33 -0300

    add host

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 const port = process.env.PORT || 8080
+const host = "10.18.83.41"
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(port, () => {
+  server.listen(port, host, () => {
     console.log(`%s listening at ${port}`);
   });
 }); 
