2d8e3e498bc73ac118213a8dd876f1f2fd5c6999
EdgarGoPr <egdeprada@hotmail.com>
2023-09-07 01:15:39 -0300

    fix help email handler

--- src/handlers/post/help_email.js
+++ src/handlers/post/help_email.js
@@ -2,9 +2,9 @@ const { helpEmail } = require("../../controllers/post/email_help")
 
 
 const emailHelp = async (req, res) => {
-    const { id, help } = req.body
+    const { email, help } = req.body
     try {
-        await helpEmail(id, help)
+        await helpEmail(email, help)
         res.send('Email enviado exitosamente!')
     } catch (error) {
         res.json({ error: error.message })
